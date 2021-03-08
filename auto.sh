ca7046b77507a1bceae74891440a49ed740900e6
ArtisticZhao <hkcreedblacktea@hotmail.com>
2021-03-08 15:17:03 +0800

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 export DISPLAY=:0
 cd /YOUR/WORK/DIR/
-date >> main.log
-python3 main.py id1 passwd1 >> main.log
+# merge the old log
+date > main.log
+# 2>&1 redirct the errors to normal output stream
+python3 main.py id1 passwd1 >> main.log 2>&1
 echo "===========" >> main.log
